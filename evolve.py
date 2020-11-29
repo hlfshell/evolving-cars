54b293794caf2d63dc3dc704095ed457dd2616d1
Keith Chester <kchester@gmail.com>
2020-11-28 19:15:57 -0800

    attempting new map

--- evolve.py
+++ evolve.py
@@ -1,6 +1,6 @@
 from src.game.game import Game, EVOLVE_MODE
 
-game = Game("assets/track1.png", mode=EVOLVE_MODE, cars_per_generation=50)
+game = Game("assets/track2.png", mode=EVOLVE_MODE, cars_per_generation=50)
 game._show_checkpoints = True
 game.init()
 game.setup()
